2d636259e6ace3d1f7054024763daadc7628bb74
Pavithra S <pavithra.s1@tataelxsi.co.in>
2025-10-29 10:59:40 +0530

    Second commit

--- src/main/java/in/pavi/controller/HelloController.java
+++ src/main/java/in/pavi/controller/HelloController.java
@@ -1,4 +1,10 @@
 package in.pavi.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    public String displayMessage(){
+        return "Successfully pushed";
+    }
 }
